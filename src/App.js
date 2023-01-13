ced0bd136f0271cd5372a7d7e18eb751c991bc4e
Andrew Park <therealandrewpark@gmail.com>
2023-01-12 19:17:11 -0800

    made the basic components for the site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './Components/Header';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Projects from './Components/Projects';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Navbar/>
+      <Hero/>
+      <Projects/>
+      <Footer/>
     </div>
   );
 }
